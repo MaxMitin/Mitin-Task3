8f90220641259bcfb6de7669a6a7c468049c01c7
MaxMitin <maksim.mitin@mail.ru>
2023-08-08 12:02:29 +0300

    Добавлен союз "ИЛИ".

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,27 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+import java.io.*;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        //Спросить пользователя "как тебя зовут?"
+        //прочитать его имя
+        //Если имя заканчивается на а, я или и, вывести "Приветик!", иначе выевсти "Здарова!"
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Введите Ваше имя!");
+        Scanner sc = new Scanner(System.in);
+        String fname = sc.next();
+        //System.out.println("Вы ввели имя: " + fname + ".");
+
+
+        String string = fname;
+        String x = string.substring(string.length() - 1);
+        if (x.equals("а") || x.equals("я") || x.equals("и")) System.out.println("Приветик!");
+        else System.out.println("Здарова!");
     }
-}
+    }
+
+
+
+
+
+
